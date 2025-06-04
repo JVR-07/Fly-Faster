08ac17315c603e297d98c740bb4ff8354d079269
JVR-07 <machado.jvr07@gmail.com>
2025-06-03 19:14:00 -0700

    Updating App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+//Dependiences
 import { useState } from 'react'
-import './App.css'
 import { Routes, Route } from 'react-router-dom'
+
+//styles
+import './App.css'
 
 //Views
 import Landing from './landing-view/Landing.jsx'
 import Client from './client-view/pages/Home.jsx'
+import Admin from './admin-view/pages/Dashboard.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Landing/>}/>
+      <Route path='/client/*' element={<Client/>}/>
+      <Route path='/admin/*' element={<Admin/>}/>
     </Routes>
   )
 }
